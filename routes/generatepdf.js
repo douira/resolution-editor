875cc95456c89125f99dd5371994818c55add33b
douira <douira100@gmail.com>
2017-07-24 19:15:24 +0200

    use double quotes instead

--- routes/generatepdf.js
+++ routes/generatepdf.js
@@ -1,9 +1,10 @@
 /*jshint asi: false, esnext: true, node: true, indent: 2*/
-const express = require('express');
+const express = require("express");
 const router = module.exports = express.Router();
+const latex = require("latex");
 
 /* POST generate pdf. */
-router.post('/', function(req, res, next) {
+router.post("/", function(req, res, next) {
   console.log(req.body);
-  //res.render('index');
+  //res.render("index");
 });
