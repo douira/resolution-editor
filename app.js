4a82c71378bd3fa015ff3a24bb183f0b45ee352b
douira <douira100@gmail.com>
2017-09-11 18:00:14 +0200

    use static resource compression

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 /*jshint asi: false, esnext: true, node: true, indent: 2*/
 const express = require("express");
 const path = require("path");
+const compression = require("compression");
 const favicon = require("serve-favicon");
 const logger = require("morgan");
 const cookieParser = require("cookie-parser");
@@ -17,6 +18,7 @@ app.set("view engine", "pug");
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
+app.use(compression());
 app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
