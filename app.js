c5c2de4836820d4a8d7476b20175df79566c22eb
douira <douira100@gmail.com>
2017-09-16 21:21:49 +0200

    reorder favicon serve in stack

--- app.js
+++ app.js
@@ -12,35 +12,35 @@ const generatePdf = require("./routes/generatepdf");
 
 const app = express();
 
-// view engine setup
+//view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
 app.use(compression());
 app.use(logger("dev"));
+app.use(favicon(path.join(__dirname, "public/favicon", "favicon.ico")));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.use(favicon(path.join(__dirname, "public/favicon", "favicon.ico")));
 
 app.use("/", index);
 app.use("/generatepdf", generatePdf);
 
-// catch 404 and forward to error handler
+//catch 404 and forward to error handler
 app.use(function(req, res, next) {
   const err = new Error("Not Found");
   err.status = 404;
   next(err);
 });
 
-// error handler
+//error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+  //set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
+  //render the error page
   res.status(err.status || 500);
   res.render("error");
 
