5291aae296dd1d31de66428c56596835104c4229
douira <douira100@gmail.com>
2018-01-27 00:26:28 +0100

    Basic print queue display

--- public/js/printQueueInit.js
+++ public/js/printQueueInit.js
@@ -6,6 +6,9 @@ var presetCode;
 //true when the check now link is alowed to work, precents click spamming
 var allowCheckNow = true;
 
+//the template element for the list
+var templateItem;
+
 //removes the topmost element from the list and updates the list
 function removeTopItem() {
   //remove top item
@@ -13,6 +16,50 @@ function removeTopItem() {
 
   //update list to make new top item
   updateList();
+}
+
+//returns a nice time text description
+function getTimeText(time) {
+  //super short
+  if (time < 30) {
+    return "Just now";
+  }
+
+  //really short
+  if (time < 60) {
+    return "Less than a minute";
+  }
+
+  //pretty short
+  if (time < 120) {
+    return "Two minutes";
+  }
+
+  //less than 120 minutes
+  time /= 60;
+  if (time < 120) {
+    return Math.round(time) + " minutes";
+  }
+
+  //less than 48 hours
+  time /= 60;
+  if (time < 48) {
+    return Math.round(time) + " hours";
+  }
+
+  //really long: any amount of days
+  return Math.round(time / 24) + " days";
+}
+
+//sets the basic attributes of a list item with a given data object
+function setBasicAttribs(data, elem) {
+  //set content in subelements
+  elem.find(".item-token").text(data.token);
+  elem.find(".item-year").text(data.idYear);
+  elem.find(".item-id").text(data.resolutionId);
+  console.log(data);
+  //set age, get delta time in seconds and convert to time text
+  elem.find(".item-age").text(getTimeText((Date.now() - data.waitTime) / 1000));
 }
 
 //updates the list of resolutions
@@ -37,7 +84,25 @@ function updateList() {
       //get first item
       var first = data.pop();
 
-      //set to display in
+      //set display of first item in special first item box
+      var firstElem = $("#first-item");
+
+      //set basic attribs
+      setBasicAttribs(first, firstElem);
+
+      //set special attributes for header element
+      var address = first.content.resolution.address;
+      firstElem.find(".item-forum").text(address.forum);
+      firstElem.find(".item-sponsor").text(address.sponsor.main);
+
+      //remove all list items that exceed the amount of items in the list
+      list.children(".list-item").slice(data.length).remove();
+
+      //set data in the left over elements
+      list.children(".list-item").each(function(index) {
+        //set attributes of this item
+        setBasicAttribs(data[index], $(this));
+      });
     } else {
       //hide list (makes an ugly line otherwise) and show no items message
       list.hide();
@@ -57,6 +122,9 @@ function updateList() {
 $(document).ready(function() {
   //get code from document
   presetCode = $("#code-preset").text();
+
+  //detach the template element from the list
+  templateItem = $("#item-template").detach().removeClass(".hide-this");
 
   //do initial list update
   updateList();
--- routes/queue.js
+++ routes/queue.js
@@ -46,15 +46,24 @@ router.post("/print/getitems", function(req, res) {
       token: 1,
       stageHistory: 1,
       resolutionId: 1,
-      yearId: 1,
-      "content.address": 1,
+      idYear: 1,
+      "content.resolution.address": 1,
       _id: 0
     }).sort({
       //sort by time in stage 4 (most necessary first), index 4 becase 0 is also a stage
       "stageHistory.4": 1
     }).toArray().then(items => {
-      //send data to client
-      res.send(items);
+      //send data to client, rewrite stageHistory
+      res.send(items.map(i => {
+        //set new property with value at index 4
+        i.waitTime = i.stageHistory[4];
+
+        //remove stageHistory
+        delete i.stageHistory;
+
+        //return original object
+        return i;
+      }));
     }, err => issueError(res, 500, "could not query print queue items", err));
   });
 });
