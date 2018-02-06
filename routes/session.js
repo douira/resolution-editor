3930bf3d3b039d1025a560d934f7fa2d8233b06f
douira <douira100@gmail.com>
2018-02-06 21:13:04 +0100

    made session work like a code entry

--- routes/session.js
+++ routes/session.js
@@ -1,26 +1,30 @@
 /*jshint esversion: 6, node: true */
 const express = require("express");
 const router = module.exports = express.Router();
-const resUtil = require("../lib/resUtil");
+const routingUtil = require("../lib/routingUtil");
 
-const issueError = resUtil.issueError;
+//const issueError = resUtil.issueError;
 
 //GET display enter access code page
 router.get("/login", function(req, res) {
-  //render login page with code input
-  res.render("login");
+  //render login page with code input, notfiy if already logged in
+  res.render("login", { loggedIn: typeof req.session.code === "string" });
 });
 
 //POST access code to auth
 router.post("/open", function(req, res) {
   //do code auth and require admin access level
-  resUtil.checkCodeStaticPerm(req, res, "admin", codeDoc => {
+  routingUtil.checkCodeStaticPerm(req, res, "admin", codeDoc => {
     //register permission in session
+    req.session.doc = codeDoc;
     req.session.code = codeDoc.code;
+
+    //extend with field session to make sure we can distinguish normal and session code docs
+    req.session.doc.isSessionDoc = true;
 
     //redirect to where the get query param backto refers to, use /list/overview as default
     res.redirect(req.query.backto || "/list/overview");
-  }, codeDoc => res.render("weakperm", { type: "adminlogin" })); //render weakperm instead
+  }, () => res.render("weakperm", { type: "adminlogin" })); //render weakperm instead
 });
 
 //GET closes login and directs to login page
