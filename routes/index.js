d6961c4c4df3584521f880a1002bc5a40358cc01
douira <douira100@gmail.com>
2018-02-08 15:52:04 +0100

    respond with node env for debugging

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,16 @@
 const express = require("express");
 const router = module.exports = express.Router();
 
-//GET help page
+//GET front page
 router.get("/", function(req, res) {
   //render page with token for resolution
   res.render("index", { promo: true });
+});
+
+//respond with env variable, for checking if the env was set correctly
+//updated every tiem the server starts, doesn't change more than that anyway
+const nodeEnv = process.env.NODE_ENV;
+router.get("/env", function(req, res) {
+  //send env as plain text
+  res.send("NODE_ENV=" + nodeEnv);
 });
