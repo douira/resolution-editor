9d65fffbcec0aad52add74f6ea2555554fa97600
douira <douia100@gmail.com>
2018-05-30 17:47:00 +0200

    fix last

--- routes/list.js
+++ routes/list.js
@@ -454,7 +454,7 @@ router.get("/print/getitems", function(req, res) {
     err => issueError(req, res, 500, "could not query print queue items", err)
   ), extDataPromise]).then(results => {
     //send data to client, rewrite and address
-    res.send(mapListItems(results[0], 4, results[1]));
+    res.send(mapListItems(results[0], 4, results[1].forums));
   });
 });
 
