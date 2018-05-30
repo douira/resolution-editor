dd78b1e3f8337f5c53d2987e5e0aa89191a0d409
douira <douia100@gmail.com>
2018-05-30 17:58:18 +0200

    add error message, debug

--- routes/list.js
+++ routes/list.js
@@ -453,10 +453,10 @@ router.get("/print/getitems", function(req, res) {
   }).toArray().catch(
     err => issueError(req, res, 500, "could not query print queue items", err)
   ), extDataPromise]).then(results => {
-    console.log(results, results[0].content);
+    console.log(results, results[0][0].content);
     //send data to client, rewrite and address
     res.send(mapListItems(results[0], 4, results[1].forums));
-  });
+  }, () => issueError(req, res, 500, "could not prepare print queue items"));
 });
 
 //uses session auth for FC queue
